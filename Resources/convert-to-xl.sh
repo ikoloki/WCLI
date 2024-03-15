d95d332bc01d3e4cc060e9201a758960bad1a232
ikoloki <me4348921@gmail.com>
2024-03-15 00:55:41 -0400

    fixed shell script to convert to csv propery

--- Resources/convert-to-xl.sh
+++ Resources/convert-to-xl.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/bash
 
-if [[ $# -eq 0 ]]; then
-   echo 'You need to specify the input file extension'
-   exit 1
-fi
-
 for file in $(find . -type f ! -name '*.pdf' ! -name '*.html' ! -name '*.sh'); do
     extension=$(echo "$file" | awk -F. '{print $NF}')
-    filename=$(echo "$file" | awk -F. '{print $1}')
-    
-    if [[ $extension == "xls" ]]; then
+    filename=$(echo "$file" | awk -F. '{print $2}')
+	
+    if [[ $extension == "csv" ]]; then
         continue
     fi
-    ssconvert "$file" "$filename.xls" "Gnumeric_Excel:excel_biff5"
+    
+    ssconvert "$file" "./${filename}.csv" --export-type=Gnumeric_stf:stf_csv
 done
-
