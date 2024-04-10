019e75640079100c592de97ae58c0d89f28c9b18
ikoloki <me4348921@gmail.com>
2024-04-10 17:31:43 -0400

    works

--- build/build.sh
+++ build/build.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 
-awk -F'|' '{print $2","$4}' ../readme.org | tail -n +2 > CSV/resource_link_table.csv
+awk -F'|' '{print $2","$4}' ../readme.org | tail -n +4 > ../build/CSV/resource_link_table.csv
 
-IFS=','
+[ -d "../resources/" ] && rm -rf "../resources/"
+
 codes=()
 links=()
 
-tail -n +4 CSV/resource_link_table.csv |
-	while read -r code link; do
-    code=$(echo "$code" | tr -d ' ')
-    link=$(echo "$link" | tr -d ' ')
-    codes+=("$code")
-    links+=("$link")
-done
+while IFS=',' read -r code link; do
+  code=$(echo "$code" | tr -d ' ')
+  link=$(echo "$link" | tr -d ' ')
+  codes+=("$code")
+  links+=("$link")
+done < ../build/CSV/resource_link_table.csv
 
-gecho Making resources dir
-[ -d ../resources/ ] && mkdir ../resources/
+if [ ! -d "../resources/" ]; then
+	echo Making resources dir
+	mkdir ../resources
+fi
 
 i=0
-for dir in ${codes[@]}; do	
-	curl -o ../resources/${codes[i]}/${codes[i]} ${links[i]}  > /dev/null 2>&1
-	[ -d ../resources/ ] && echo "creating ../resources/${codes[i]}" ;  mkdir ../resources/${codes[i]}
-	ext=$(file --extension ../resources/${codes[i]}/${codes[i]} | awk '{print $NF}')
-	mv "../resources/${codes[i]}/${codes[i]}" "../resources/${codes[i]}/${codes[i]}.${ext}"
-	echo Created "../resources/${codes[i]}/${codes[i]}.${ext}"
+for code in "${codes[@]}"; do
+	if [ ! -d "../resources/${codes[i]}" ]; then
+		if [ ! -z "${links[i]}" ]; then
+			echo -e "creating ../resources/${codes[i]}"
+			mkdir "../resources/${codes[i]}"
+			curl -L -o "../resources/${codes[i]}/${codes[i]}" "${links[i]}" 
+			ext=$(file --extension "../resources/${codes[i]}/${codes[i]}" | awk '{print $NF}')
+			
+			mv "../resources/${codes[i]}/${codes[i]}" "../resources/${codes[i]}/${codes[i]}.${ext}"
+			echo -e Created "../resources/${codes[i]}/${codes[i]}.${ext}"
+		fi
+	fi
 	((i++))
 done
 
 ./convert2csv.sh
+mkdir ../resources/Abortion/Clean
+./../src/clean_abortion_data.awk ../resources/Abortion/Abortion.csv > ../resources/Abortion/Clean/SumAborition.csv
+
