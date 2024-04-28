e03bcb749e52b44c81e699620e76fc9f149d5ca3
ikoloki <me4348921@gmail.com>
2024-04-28 18:13:55 -0400

    build update

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-awk -F'|' '{print $2","$4}' ../readme.org | tail -n +4 > ../build/CSV/resource_link_table.csv
-
+awk -F'|' '{print $2","$4}' ../readme.org | tail -n +4 > ../build/resource_link_table.csv
 [ -d "../resources/" ] && rm -rf "../resources/"
 
 codes=()
@@ -12,7 +11,7 @@ while IFS=',' read -r code link; do
   link=$(echo "$link" | tr -d ' ')
   codes+=("$code")
   links+=("$link")
-done < ../build/CSV/resource_link_table.csv
+done  < ../build/resource_link_table.csv
 
 if [ ! -d "../resources/" ]; then
 	echo Making resources dir
@@ -25,9 +24,8 @@ for code in "${codes[@]}"; do
 		if [ ! -z "${links[i]}" ]; then
 			echo -e "creating ../resources/${codes[i]}"
 			mkdir "../resources/${codes[i]}"
-			curl -L -o "../resources/${codes[i]}/${codes[i]}" "${links[i]}" 
+			curl -L -o "../resources/${codes[i]}/${codes[i]}" "${links[i]}"
 			ext=$(file --extension "../resources/${codes[i]}/${codes[i]}" | awk '{print $NF}')
-			
 			mv "../resources/${codes[i]}/${codes[i]}" "../resources/${codes[i]}/${codes[i]}.${ext}"
 			echo -e Created "../resources/${codes[i]}/${codes[i]}.${ext}"
 		fi
@@ -35,7 +33,24 @@ for code in "${codes[@]}"; do
 	((i++))
 done
 
-./convert2csv.sh
-mkdir ../resources/Abortion/Clean
-./../src/clean_abortion_data.awk ../resources/Abortion/Abortion.csv > ../resources/Abortion/Clean/SumAborition.csv
+echo "Making Clean directory"
+mkdir '../resources/Clean'
 
+echo "Processing Happiness"
+./scripts/./happy.awk "../resources/Happiness/Happiness.???" > ../resources/Clean/clean-Happiness.csv
+
+echo "Processing Aborton"
+mv ../resources/Abortion/Abortion.xlsx ../resources/Clean/clean-Abortion.xlsx
+
+echo "Processing Life Expectancy"
+./scripts/./life_expect.awk ../resources/LifeExpectancy/LifeExpectancy.??? > ../resources/Clean/clean-LifeExpectancy.csv
+
+echo "Processing Pregnancy"
+pdftohtml ../resources/Pregnancy/Pregnancy.pdf -stdout | ./scripts/./Pregnacy.awk > ../resources/Clean/clean-Pregnancy.csv
+
+scripts/./convert2csv.sh
+
+echo "Processing Abortion Data"
+scripts/./Abortion.R
+
+../src//./clean_data.R
